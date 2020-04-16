ded946489658a467ced30847eb7f195f91cc88f9
Nisha Bhat <nisha.vb@gmail.com>
2020-04-15 18:38:59 -0700

    budget planner app added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
-
+import { AlertController } from "@ionic/angular";
+import { Component } from "@angular/core";
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
+  input_reason: string;
+  input_amount: number;
+  totalExpenses = 0;
+  dataList = [];
+  constructor(public alertController: AlertController) {}
 
-  constructor() {}
-
+  addExpense() {
+    if (this.input_reason.trim().length <= 0 || this.input_amount <= 0) return;
+    this.dataList.push(`${this.input_reason}:$${this.input_amount}`);
+    this.totalExpenses += this.input_amount;
+    this.clear();
+  }
+  clear() {
+    this.input_amount = 0;
+    this.input_reason = "";
+  }
 }
